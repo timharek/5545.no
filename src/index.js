3a5ff702eef25eadf0b410fa39572f732a07ad4d
Tim <tim@adipid.no>
2020-04-25 15:58:09 +0200

    More data for the forecasts #2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
+        date: '',
         temperature: '',
+        wind: '',
+        rainMin: '',
+        rainMax: '',
         weatherType: '',
         weatherImg: '',
         forecasts: []
     },
-
     methods: {
         getWeatherData: async function(url) {
             const response = await fetch(url, {
@@ -23,32 +26,72 @@ var app = new Vue({
 
             //console.log(localData.product)
 
+            this.date = localData.created
             this.temperature = localData.product.time[0].location.temperature.value
+            this.wind = localData.product.time[0].location.windSpeed.mps
+            this.rainMin = localData.product.time[1].location.precipitation.minvalue
+            this.rainMax = localData.product.time[1].location.precipitation.maxvalue
+
             this.weatherType = localData.product.time[1].location.symbol.id
-            this.weatherImg = 'https://api.met.no/weatherapi/weathericon/1.1/?content_type=image%2Fpng&symbol=' + localData.product.time[1].location.symbol.number
+            this.weatherImg =  this.getWeatherImage(localData.product.time[1].location.symbol.number)
 
             this.getForecast(localData.product)
         },
         getCustomWeather: function() {
             this.getWeather(this.$refs.lat.value, this.$refs.lon.value)
         },
+        getWeatherImage: function(weatherId) {
+            return 'https://api.met.no/weatherapi/weathericon/1.1/?content_type=image%2Fpng&symbol=' + weatherId
+        },
         getForecast: function(forecast) {
             console.log(forecast)
+
+            var locForecastsEven = []
+            var locForecastsOdd = []
 
             // Three hour forecast
             for (var i = 4; i < 10; i++) {
                 if ((i % 2 == 0)) {
-                    this.forecasts.push({ 
+                    locForecastsEven.push({ 
                         temp: forecast.time[i].location.temperature.value,
                         time: forecast.time[i].to,
+                        /* weather: '', */
+                        wind: forecast.time[i].location.windSpeed.mps,
+                        wind_desc: forecast.time[i].location.windSpeed.name,
+                        /* rain: '', */
                     })
-                } 
+                } else {
+                    locForecastsOdd.push({
+                        weather: this.getWeatherImage(forecast.time[i].location.symbol.number),
+                        rain: this.getAverageRain(forecast.time[i].location.precipitation.minvalue, forecast.time[i].location.precipitation.maxvalue),
+                    })
+                }
             }
 
+            for (var i = 0; i < locForecastsOdd.length; i++) {
+                locForecastsOdd[i] = {
+                    temp: locForecastsEven[i].temp, 
+                    time: locForecastsEven[i].time, 
+                    wind: locForecastsEven[i].wind, 
+                    wind_desc: locForecastsEven[i].wind_desc, 
+                    weather: locForecastsOdd[i].weather, 
+                    rain: locForecastsOdd[i].rain}
+            }
+
+            locForecastsOdd.forEach(forecast => {
+                this.forecasts.push(forecast)
+            });
+
             console.log(this.forecasts)
+        },
+        getAverageRain: function(rainMin, rainMax) {
+            return (Number(rainMax) + Number(rainMin)) / 2
         }
     },
     beforeMount() {
         this.getWeather('59.355091', '5.323378') // Vormedal
+        Array.prototype.insert = function (index, item) {
+            this.splice( index, 0, item )
+        }
     }
 })
