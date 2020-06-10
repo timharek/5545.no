9570f30a811a48d8f7b4c46c055989217407ca24
Tim <tim@harek.dev>
2020-06-10 22:25:52 +0200

    Started working on new 2.0 API calls #7
    Also began with updating to the new design #4

--- src/index.js
+++ src/index.js
@@ -1,182 +1,68 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        date: '',
-        temperature: '',
-        wind: '',
-        rainMin: '',
-        rainMax: '',
-        weatherType: '',
-        weatherImg: '',
-        nextThreeHours: [],
-        forecastNextDays: [],
+  el: "#app",
+  data: {
+    current_date: "",
+    current_temperature: "",
+    current_wind: "",
+    current_rainMin: "",
+    current_rainMax: "",
+    current_rain: "",
+    current_weatherType: "",
+    current_weatherImg: "",
+    nextThreeHours: [],
+    forecastNextDays: [],
+  },
+  methods: {
+    getWeatherData: async function (url) {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      return response.json();
     },
-    methods: {
-        getWeatherData: async function(url) {
-            const response = await fetch(url, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json'
-                }
-            })
+    getWeather: async function (lat, lon) {
+      const tenMin = 1000 * 60 * 10;
+      if (
+        localStorage.getItem("localData") == null ||
+        Date.now() - localStorage.getItem("time") > tenMin
+      ) {
+        console.log("Getting new weather data...");
+        const localData = await this.getWeatherData(
+          "https://api.met.no/weatherapi/locationforecast/2.0/.json?lat=" +
+            lat +
+            "&lon=" +
+            lon +
+            ""
+        );
+        localStorage.setItem("localData", JSON.stringify(localData));
+        localStorage.setItem("time", Date.now());
+      }
+      var retrivedData = localStorage.getItem("localData");
 
-            return response.json()
-        },
-        getWeather: async function(lat, lon) {
-            const tenMin = 1000 * 60 * 10;
-            if (localStorage.getItem('localData') == null || Date.now() - localStorage.getItem('time') > tenMin) {
-                console.log("Getting new weather data...")
-                const localData = await this.getWeatherData('https://api.met.no/weatherapi/locationforecast/1.9/.json?lat='+ lat + '&lon='+ lon + '')
-                localStorage.setItem('localData', JSON.stringify(localData))
-                localStorage.setItem('time', Date.now())
-            }
-            var retrivedData = localStorage.getItem('localData')
-
-            this.setWeather(JSON.parse(retrivedData).product)
-        },
-        getCustomWeather: function() {
-            this.getWeather(this.$refs.lat.value, this.$refs.lon.value)
-        },
-        setWeather: function(weatherData) {
-            this.date = this.getTodaysDate()
-            this.temperature = Number(weatherData.time[0].location.temperature.value)
-            this.wind = this.roundWindSpeed(weatherData.time[0].location.windSpeed.mps)
-            this.rainMin = Number(weatherData.time[1].location.precipitation.minvalue)
-            this.rainMax = Number(weatherData.time[1].location.precipitation.maxvalue)
-
-            this.weatherType = weatherData.time[1].location.symbol.id
-            this.weatherImg =  this.getWeatherImage(weatherData.time[1].location.symbol.number)
-
-            this.setForecastNextThreeHours(weatherData)
-            this.findForecastForNextDays(weatherData)
-        },
-        getWeatherImage: function(weatherId, time) {
-            if (time >= 6) {
-                return 'https://api.met.no/weatherapi/weathericon/1.1/?content_type=image%2Fpng&symbol=' + weatherId
-            } else {
-                return 'https://api.met.no/weatherapi/weathericon/1.1/?content_type=image%2Fpng&symbol=' + weatherId + '&is_night=1'
-            }
-        },
-        setForecastNextThreeHours: function(forecast) {
-            // This is for the Met.no API
-            var nextThreeHoursEven = []
-            var nextThreeHoursOdd = []
-
-            console.log(this.cleanTime(forecast.time[0].to))
-
-            // Three hour forecast
-            for (var i = 4; i < 10; i++) {
-                if ((i % 2 == 0)) {
-                    nextThreeHoursEven.push({ 
-                        temp: Number(forecast.time[i].location.temperature.value),
-                        time: this.cleanTime(forecast.time[i].to),
-                        wind: this.roundWindSpeed(forecast.time[i].location.windSpeed.mps),
-                        wind_desc: forecast.time[i].location.windSpeed.name,
-                    })
-                } else {
-                    var rain = this.roundRain(this.getAverageRain(forecast.time[i].location.precipitation.minvalue, forecast.time[i].location.precipitation.maxvalue))
-                    nextThreeHoursOdd.push({
-                        weather: this.getWeatherImage(forecast.time[i].location.symbol.number, this.cleanTime(forecast.time[i].to)),
-                        rain: rain,
-                    })
-                }
-            }
-
-            for (var i = 0; i < nextThreeHoursOdd.length; i++) {
-                nextThreeHoursOdd[i] = {
-                    temp: nextThreeHoursEven[i].temp, 
-                    time: nextThreeHoursEven[i].time, 
-                    wind: nextThreeHoursEven[i].wind, 
-                    wind_desc: nextThreeHoursEven[i].wind_desc, 
-                    weather: nextThreeHoursOdd[i].weather, 
-                    rain: nextThreeHoursOdd[i].rain}
-            }
-
-            nextThreeHoursOdd.forEach(forecast => {
-                this.nextThreeHours.push(forecast)
-            });
-        },
-        findForecastForNextDays: function(forecast) {
-            var date = this.getLocalDate()
-
-            var nextDays = []
-            var moreThan1Hour = []
-
-            for (var i = 0; i < forecast.time.length; i++) {
-                if (forecast.time[i].from.substring(0, 10) != date) {
-                    nextDays.push(forecast.time[i].from)
-                }
-            }
-
-            for (var i = 0; i < forecast.time.length; i++) {
-                var from = forecast.time[i].from.substring(11, 13)
-                var to = forecast.time[i].to.substring(11, 13)
-                var difference = Number(to) - Number(from)
-
-                if (date != forecast.time[i].to.substring(0, 10)) {
-                    if (difference > 1) {
-                        moreThan1Hour.push(i)
-                    }
-                }
-            }
-
-            console.log(nextDays)
-            console.log(moreThan1Hour)
-            console.log(new Date().toLocaleDateString())
-            console.log(date)
-
-            this.setForecastNextDays(forecast, moreThan1Hour)
-        },
-        setForecastNextDays: function(forecast, indices) {
-            console.log(indices[0])
-            console.log(forecast.time[indices[0]].location.minTemperature.value)
-
-            var tempArr = []
-            for (var index = 0; index < forecast.time.length; index++) {
-                var fulldate = String(forecast.time[indices[index]].from).substring(5, 10)
-                var date = fulldate.split('-')
-                this.forecastNextDays.push({
-                    date: date[1] + '-' + date[0],
-                    time: this.cleanTime(forecast.time[indices[index]].from),
-                    tempMax: forecast.time[indices[index]].location.maxTemperature.value,
-                    tempMin: forecast.time[indices[index]].location.minTemperature.value,
-                    weather: this.getWeatherImage(forecast.time[indices[index]].location.symbol.number, this.cleanTime(forecast.time[indices[index]].from)),
-                    wind: this.roundWindSpeed(forecast.time[indices[index]+1].location.windSpeed.mps),
-                    wind_desc: forecast.time[indices[index]+1].location.windSpeed.name,
-                })
-            }
-        },
-        getAverageRain: function(rainMin, rainMax) {
-            return (Number(rainMax) + Number(rainMin)) / 2
-        },
-        getLocalDate: function() {
-            // Get the local date in ISO format
-            var dateTemp = new Date();
-            dateTemp.setMinutes(dateTemp.getMinutes() - dateTemp.getTimezoneOffset());
-            return dateTemp.toISOString().substring(0, 10);
-        },
-        cleanTime: function(time) {
-            var result = time.substring(11)
-            result = result.slice(0, -7)
-            return result
-        },
-        roundRain: function(number) {
-            return Math.round(Number(number) * 10) / 10
-        },
-        roundWindSpeed: function(number) {
-            return Math.round(Number(number))
-        },
-        getTodaysDate: function() {
-            var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-            var date = new Date()
-            
-            return date.toLocaleDateString(undefined, options)
-        }
+      this.setWeather(JSON.parse(retrivedData).properties);
     },
-    beforeMount() {
-        this.getWeather('59.355091', '5.323378') // Vormedal
-        Array.prototype.insert = function (index, item) {
-            this.splice( index, 0, item )
-        }
-    }
-})
+    setWeather: function (weatherData) {
+      this.current_weatherType =
+        weatherData.timeseries[0].data.next_1_hours.summary.symbol_code;
+      this.current_temperature =
+        weatherData.timeseries[0].data.instant.details.air_temperature;
+      this.current_rain =
+        weatherData.timeseries[0].data.next_1_hours.details.precipitation_amount;
+      this.current_wind =
+        weatherData.timeseries[0].data.instant.details.wind_speed;
+      this.current_weatherImg = this.getWeatherImage(this.current_weatherType);
+    },
+    getWeatherImage: function (weatherType) {
+      return "img/weathericons/" + weatherType + ".svg";
+    },
+  },
+  beforeMount() {
+    this.getWeather("59.355091", "5.323378"); // Vormedal
+    Array.prototype.insert = function (index, item) {
+      this.splice(index, 0, item);
+    };
+  },
+});
---
+++ src/index19.js
@@ -0,0 +1,228 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    date: "",
+    temperature: "",
+    wind: "",
+    rainMin: "",
+    rainMax: "",
+    weatherType: "",
+    weatherImg: "",
+    nextThreeHours: [],
+    forecastNextDays: [],
+  },
+  methods: {
+    getWeatherData: async function (url) {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      return response.json();
+    },
+    getWeather: async function (lat, lon) {
+      const tenMin = 1000 * 60 * 10;
+      if (
+        localStorage.getItem("localData") == null ||
+        Date.now() - localStorage.getItem("time") > tenMin
+      ) {
+        console.log("Getting new weather data...");
+        const localData = await this.getWeatherData(
+          "https://api.met.no/weatherapi/locationforecast/1.9/.json?lat=" +
+            lat +
+            "&lon=" +
+            lon +
+            ""
+        );
+        localStorage.setItem("localData", JSON.stringify(localData));
+        localStorage.setItem("time", Date.now());
+      }
+      var retrivedData = localStorage.getItem("localData");
+
+      this.setWeather(JSON.parse(retrivedData).product);
+    },
+    getCustomWeather: function () {
+      this.getWeather(this.$refs.lat.value, this.$refs.lon.value);
+    },
+    setWeather: function (weatherData) {
+      this.date = this.getTodaysDate();
+      this.temperature = Number(weatherData.time[0].location.temperature.value);
+      this.wind = this.roundWindSpeed(
+        weatherData.time[0].location.windSpeed.mps
+      );
+      this.rainMin = Number(
+        weatherData.time[1].location.precipitation.minvalue
+      );
+      this.rainMax = Number(
+        weatherData.time[1].location.precipitation.maxvalue
+      );
+
+      this.weatherType = weatherData.time[1].location.symbol.id;
+      this.weatherImg = this.getWeatherImage(
+        weatherData.time[1].location.symbol.number
+      );
+
+      this.setForecastNextThreeHours(weatherData);
+      this.findForecastForNextDays(weatherData);
+    },
+    getWeatherImage: function (weatherId, time) {
+      if (time >= 6) {
+        return (
+          "https://api.met.no/weatherapi/weathericon/1.1/?content_type=image%2Fpng&symbol=" +
+          weatherId
+        );
+      } else {
+        return (
+          "https://api.met.no/weatherapi/weathericon/1.1/?content_type=image%2Fpng&symbol=" +
+          weatherId +
+          "&is_night=1"
+        );
+      }
+    },
+    setForecastNextThreeHours: function (forecast) {
+      // This is for the Met.no API
+      var nextThreeHoursEven = [];
+      var nextThreeHoursOdd = [];
+
+      console.log(this.cleanTime(forecast.time[0].to));
+
+      // Three hour forecast
+      for (var i = 4; i < 10; i++) {
+        if (i % 2 == 0) {
+          nextThreeHoursEven.push({
+            temp: Number(forecast.time[i].location.temperature.value),
+            time: this.cleanTime(forecast.time[i].to),
+            wind: this.roundWindSpeed(forecast.time[i].location.windSpeed.mps),
+            wind_desc: forecast.time[i].location.windSpeed.name,
+          });
+        } else {
+          var rain = this.roundRain(
+            this.getAverageRain(
+              forecast.time[i].location.precipitation.minvalue,
+              forecast.time[i].location.precipitation.maxvalue
+            )
+          );
+          nextThreeHoursOdd.push({
+            weather: this.getWeatherImage(
+              forecast.time[i].location.symbol.number,
+              this.cleanTime(forecast.time[i].to)
+            ),
+            rain: rain,
+          });
+        }
+      }
+
+      for (var i = 0; i < nextThreeHoursOdd.length; i++) {
+        nextThreeHoursOdd[i] = {
+          temp: nextThreeHoursEven[i].temp,
+          time: nextThreeHoursEven[i].time,
+          wind: nextThreeHoursEven[i].wind,
+          wind_desc: nextThreeHoursEven[i].wind_desc,
+          weather: nextThreeHoursOdd[i].weather,
+          rain: nextThreeHoursOdd[i].rain,
+        };
+      }
+
+      nextThreeHoursOdd.forEach((forecast) => {
+        this.nextThreeHours.push(forecast);
+      });
+    },
+    findForecastForNextDays: function (forecast) {
+      var date = this.getLocalDate();
+
+      var nextDays = [];
+      var moreThan1Hour = [];
+
+      for (var i = 0; i < forecast.time.length; i++) {
+        if (forecast.time[i].from.substring(0, 10) != date) {
+          nextDays.push(forecast.time[i].from);
+        }
+      }
+
+      for (var i = 0; i < forecast.time.length; i++) {
+        var from = forecast.time[i].from.substring(11, 13);
+        var to = forecast.time[i].to.substring(11, 13);
+        var difference = Number(to) - Number(from);
+
+        if (date != forecast.time[i].to.substring(0, 10)) {
+          if (difference > 1) {
+            moreThan1Hour.push(i);
+          }
+        }
+      }
+
+      console.log(nextDays);
+      console.log(moreThan1Hour);
+      console.log(new Date().toLocaleDateString());
+      console.log(date);
+
+      this.setForecastNextDays(forecast, moreThan1Hour);
+    },
+    setForecastNextDays: function (forecast, indices) {
+      console.log(indices[0]);
+      console.log(forecast.time[indices[0]].location.minTemperature.value);
+
+      var tempArr = [];
+      for (var index = 0; index < forecast.time.length; index++) {
+        var fulldate = String(forecast.time[indices[index]].from).substring(
+          5,
+          10
+        );
+        var date = fulldate.split("-");
+        this.forecastNextDays.push({
+          date: date[1] + "-" + date[0],
+          time: this.cleanTime(forecast.time[indices[index]].from),
+          tempMax: forecast.time[indices[index]].location.maxTemperature.value,
+          tempMin: forecast.time[indices[index]].location.minTemperature.value,
+          weather: this.getWeatherImage(
+            forecast.time[indices[index]].location.symbol.number,
+            this.cleanTime(forecast.time[indices[index]].from)
+          ),
+          wind: this.roundWindSpeed(
+            forecast.time[indices[index] + 1].location.windSpeed.mps
+          ),
+          wind_desc: forecast.time[indices[index] + 1].location.windSpeed.name,
+        });
+      }
+    },
+    getAverageRain: function (rainMin, rainMax) {
+      return (Number(rainMax) + Number(rainMin)) / 2;
+    },
+    getLocalDate: function () {
+      // Get the local date in ISO format
+      var dateTemp = new Date();
+      dateTemp.setMinutes(dateTemp.getMinutes() - dateTemp.getTimezoneOffset());
+      return dateTemp.toISOString().substring(0, 10);
+    },
+    cleanTime: function (time) {
+      var result = time.substring(11);
+      result = result.slice(0, -7);
+      return result;
+    },
+    roundRain: function (number) {
+      return Math.round(Number(number) * 10) / 10;
+    },
+    roundWindSpeed: function (number) {
+      return Math.round(Number(number));
+    },
+    getTodaysDate: function () {
+      var options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      };
+      var date = new Date();
+
+      return date.toLocaleDateString(undefined, options);
+    },
+  },
+  beforeMount() {
+    this.getWeather("59.355091", "5.323378"); // Vormedal
+    Array.prototype.insert = function (index, item) {
+      this.splice(index, 0, item);
+    };
+  },
+});
